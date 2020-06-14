14c07f69734467e92ad0199a5d52f8ec7df3e06e
Leonardo Narciso <lnarcisoov@gmail.com>
2020-06-14 18:57:57 -0300

    Adding functions to delete and update user.

--- src/main/java/br/com/b3/controller/UserController.java
+++ src/main/java/br/com/b3/controller/UserController.java
@@ -1,16 +1,18 @@
 package br.com.b3.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,7 @@ import br.com.b3.service.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	@Autowired
@@ -30,7 +33,7 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping(path = "/")
-	public ResponseEntity<Object> saveUser(@Valid @RequestBody User user) {
+	public ResponseEntity<Object> save(@Valid @RequestBody User user) {
 		try {
 			userService.validationRulesToSaveUser(user) ;
 			User userdb = userRepository.save(user);
@@ -50,6 +53,23 @@ public class UserController {
 	public ResponseEntity<Object> getById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		return new ResponseEntity<Object>(user, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Object> deleteById(@PathVariable Integer id) {
+		userRepository.deleteById(id);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PutMapping(path = "/")
+	public ResponseEntity<Object> update(@Valid @RequestBody User user) {
+		try {
+			userService.validationRulesToSaveUser(user) ;
+			User userdb = userRepository.save(user);
+			return new ResponseEntity<Object>(userdb, HttpStatus.OK);
+		} catch(Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@GetMapping(path = "/company/{companyId}")
